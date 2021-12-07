3c3b75b847dd9ab104af6697ba8d1ffd0ce67a18
Tim Echtenkamp <techtenkamp@noblhealth.com>
2021-12-06 20:21:31 -0600

    puzzle 2 solved. still need to work on the generic to be able to run all the puzzles without adding it to a long list in main

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,39 @@ use std::io;
 use std::fs::File;
 use std::io::BufRead;
 use std::path::Path;
+use std::num::ParseIntError;
 
 fn main() {
 
     let file = "./Day01/puzzle_01/input_tim.txt";
 
     if let Ok(lines) = read_lines(file) {
-        Day1Puzzle1::run(lines);
+        let result = Day1Puzzle2::run(lines);
+        match result {
+            Ok(_) => (),
+            Err(_) => println!("Something went wrong!")
+        }
     }
 }
 
+struct PuzzleError {
+}
+
+impl From<ParseIntError> for PuzzleError {
+    fn from(_: ParseIntError) -> Self {
+        PuzzleError { }
+    }
+}
+
+impl From<std::io::Error> for PuzzleError {
+    fn from(_: std::io::Error) -> Self {
+        PuzzleError { }
+    }
+}
+
+
 trait Puzzle {
-    fn run (lines: io::Lines<io::BufReader<File>>);
+    fn run (lines: io::Lines<io::BufReader<File>>) -> Result<bool, PuzzleError>;
 }
 
 struct Day1Puzzle1 {
@@ -21,9 +42,9 @@ struct Day1Puzzle1 {
 }
 
 impl Puzzle for Day1Puzzle1 {
-    fn run (lines: io::Lines<io::BufReader<File>>) {
-        let mut previous :i32 = 0;
-        let mut count :i32 = -1; // the first "increase" doesn't count, so rather than doing extra checks, just start at -1
+    fn run (lines: io::Lines<io::BufReader<File>>) -> Result<bool, PuzzleError> {
+        let mut previous: i32 = 0;
+        let mut count: i32 = -1; // the first "increase" doesn't count, so rather than doing extra checks, just start at -1
         for line in lines {
             if let Ok(ok_line) = line {
                 if let Ok(number) = ok_line.parse::<i32>() {
@@ -35,6 +56,39 @@ impl Puzzle for Day1Puzzle1 {
             }
         }
         println!("{}", count);
+        Ok(true)
+    }
+}
+
+struct Day1Puzzle2 {
+
+}
+
+impl Puzzle for Day1Puzzle2 {
+    fn run (lines: io::Lines<io::BufReader<File>>) -> Result<bool, PuzzleError> {
+        let mut previous: i32 = 0;
+        let mut count: i32 = -1;
+        // a, b, and c will contain a Result, use the question mark to unwrap it and pass errors up the stack
+        // let mut a_opt = lines.nth(0);
+        // let mut a = a_opt.unwrap()?.parse::<i32>()?;
+        // let mut b = lines.nth(1).unwrap()?.parse::<i32>()?;
+        // let mut c = lines.nth(2).unwrap()?.parse::<i32>()?;
+        let mut a;
+        let mut b = 0;
+        let mut c = 0;
+        for line in lines {
+            a = b;
+            b = c;
+            c = line.unwrap().parse::<i32>()?;
+            // now we just do the same thing as Puzzle 1, except different
+            if a > 0 && (a + b + c) > previous {
+                count = count + 1;
+            }
+            previous = a + b + c;
+
+        }
+        println!("{}", count);
+        Ok(true)
     }
 }
 
