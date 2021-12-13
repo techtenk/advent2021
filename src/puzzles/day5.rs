2ddd6438a3efc351952d2a4dd9a918f933122383
Tim Echtenkamp <techtenkamp@noblhealth.com>
2021-12-13 13:51:40 -0600

    damn. wasn't nearly as hard as I made it

--- src/puzzles/day5.rs
+++ src/puzzles/day5.rs
@@ -30,23 +30,14 @@ fn count_overlaps(lines: Lines<BufReader<&[u8]>>, include_diags: bool) -> usize 
     // check each vent against all the others, return and fill out the HashMap of intersects
     // key is [x,y] and the value is the number of intersects
     let mut intersections: HashMap<[u16; 2], u8> = HashMap::new();
-    let mut points_hash: HashMap<[u16; 4], Vec<[u16; 2]>> = HashMap::new();
-    for (i, vent) in vents.iter().enumerate() {
-        // println!("working on vent: {:?}", vent);
-        if let Some(remaining_vents) = vents.get((i+1)..vents.len()) {
-            for v in remaining_vents {
-                let intersects: Vec<[u16; 2]> = get_intersects(& mut points_hash, vent, v);
-                // now we have the points that intersect, let's add/increment the HashMap
-                for i in intersects {
-                    let count = intersections.entry(i).or_insert(1);
-                    *count += 1;
-                }
-            }
+    for v in vents.iter().as_ref() {
+        for point in get_points(v) {
+            let count = intersections.entry(point).or_insert(0);
+            *count += 1;
         }
-        
     }
     // println!("{:?}", intersections);
-    intersections.len()
+    intersections.drain_filter(|_, v| *v > 1).count()
 }
 
 pub struct Day5Puzzle1 {
@@ -87,35 +78,6 @@ impl Puzzle for Day5Puzzle2 {
         
         Ok(count_overlaps(lines, true).to_string())
     }
-}
-
-fn get_intersects(points_hash: & mut HashMap<[u16; 4], Vec<[u16; 2]>>, vent1: &[u16; 4], vent2: &[u16; 4]) -> Vec<[u16; 2]> {
-    let mut intersects = Vec::new();
-    // now just check if vent1 and vent2 share any points
-    // println!("get_intersects for {:?} and {:?}", vent1, vent2);
-    
-    if !points_hash.contains_key(vent1) {
-        points_hash.insert(*vent1, get_points(vent1));
-    }
-    if !points_hash.contains_key(vent2) {
-        points_hash.insert(*vent2, get_points(vent2));
-    }
-    let mut all_points: Vec<[u16; 2]> = points_hash.get(vent1).expect("error").to_vec();
-    let vent2_points: Vec<[u16; 2]> = points_hash.get(vent2).expect("error").to_vec();
-    all_points.extend(vent2_points);
-    all_points.sort_unstable();
-    // println!("now find intersects in {:?}", all_points);
-    let mut last_point: Option<[u16; 2]> = None;
-    all_points.iter().for_each(|p| {
-        if let Some(lp) = last_point {
-            if *p == lp {
-                intersects.push(*p);
-            }
-        }
-        last_point = Some(*p);
-    });
-    // println!("intersects: {:?}", intersects);
-    intersects
 }
 
 fn get_points(vent: &[u16; 4]) -> Vec<[u16; 2]> {
