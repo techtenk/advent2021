5a9a17c555fb5df94efd35112b7dad37cd6713f5
Tim Echtenkamp <techtenkamp@noblhealth.com>
2021-12-18 06:32:23 -0600

    day 9 complete

--- src/puzzles/day9.rs
+++ src/puzzles/day9.rs
@@ -30,7 +30,7 @@ pub struct Day9Puzzle2 {
 
 impl Puzzle for Day9Puzzle2 {
     fn get_puzzle_name(&self) -> &'static str {
-        "Day 8 Puzzle 1"
+        "Day 8 Puzzle 2"
     }
     fn get_input(&self) -> Box<&'static [u8]> {
         let bytes = include_bytes!("../../Day09/input_tim.txt");
@@ -41,48 +41,55 @@ impl Puzzle for Day9Puzzle2 {
         let lines = self.get_lines();
         let height_map = build_height_map(lines);
 
-        let basins: Vec<Vec<Reading>> = Vec::new();
+        let mut basins: Vec<Vec<Reading>> = Vec::new();
 
         // walk the map and add the Readings to new basins as needed
-        for x in 0..100 {
-            for y in 0..100 {
+        for y in 0..100 {
+            for x in 0..100 {
                 let reading = height_map[x][y];
+                if reading.unwrap().height.unwrap() == 9 {
+                    continue;
+                }
                 // search all the current basins for it's neighbors, if we find any then add this reading to the same basin, otherwise create a new one
                 let neighbors: Vec<Reading> = reading.unwrap().get_neighbors();
-                let vec_to_create_or_merge: Vec<& mut Vec<Reading>> = Vec::new(); // create if this is empty, merge if it has more than one, otherwise just add
-                for b in &basins {
-                    for readings in b {
-                        if neighbors.iter().any(|neighbor| *neighbor == reading.unwrap()) {
-                            vec_to_create_or_merge.push(&mut b);
-                        }
+                let mut vec_to_create_or_merge: Vec<& mut Vec<Reading>> = Vec::new(); // create if this is empty, merge if it has more than one, otherwise just add
+                for b in basins.iter_mut() {
+                    // if any of the Readings in neighbor are also in b, then add b to vec_to_create_or_merge
+                    if neighbors.iter().any(|neighbor| b.iter().any(|item| *item == *neighbor && height_map[neighbor.x][neighbor.y].unwrap().height.unwrap() < 9)) {
+                        vec_to_create_or_merge.push(b);
                     }
                 }
 
                 // add, merge or create
                 if vec_to_create_or_merge.is_empty() {
+                    // println!("making new basin from {} {}", reading.unwrap().x, reading.unwrap().y);
                     basins.push(vec!(reading.unwrap()));
                 } else if vec_to_create_or_merge.len() > 1 {
-                    for (i, v) in vec_to_create_or_merge.iter().enumerate() {
-                        if i == 0 {
-                            continue;
-                        }
-                        vec_to_create_or_merge[0].extend(v.drain(1..))
+                    // println!("({},{}) matches more than one basin. Merge them.", x, y);
+                    let (first_basin, rest) = vec_to_create_or_merge.split_at_mut(1);
+                    
+                    for v in rest.iter_mut() {
+                        // println!("merging basin {:?} with {:?}", v, first_basin);
+                        first_basin[0].extend(v.drain(0..));
                     }
+                    // now add the reading to the merged basin
+                    first_basin[0].push(reading.unwrap());
                     // remove empty vectors
                     basins.drain_filter(|basin| basin.is_empty());
                 } else {
+                    // println!("adding {} {} to basin: {:?}", reading.unwrap().x, reading.unwrap().y, vec_to_create_or_merge[0]);
                     // exactly one, just add the Reading to it
                     vec_to_create_or_merge[0].push(reading.unwrap());
                 }
             }
         }
 
-        println!("basins: ");
-        for b in basins {
-            println!("{:?}", b);
-        }
-
-        Ok("working".to_string())
+        // sort the basins by len
+        basins.sort_unstable_by(|a, b| {
+            b.len().cmp(&(a.len()))
+        });
+        let result = basins[0].len() * basins[1].len() * basins[2].len();
+        Ok(result.to_string())
     }
 }
 
@@ -92,7 +99,7 @@ fn build_height_map(lines: Lines<BufReader<&[u8]>>) -> [[Option<Reading>; 100]; 
         let heights = l.expect("error");
         for (x, h_str) in heights.chars().enumerate() {
             let h = h_str.to_string().parse::<u32>().expect("error");
-            height_map[x][y] = Some(Reading { x, y, height: h });
+            height_map[x][y] = Some(Reading { x, y, height: Some(h) });
         }
     }
     height_map
@@ -112,14 +119,14 @@ fn assess_risk(height_map: [[Option<Reading>; 100]; 100]) -> u32 {
 struct Reading {
     x: usize,
     y: usize,
-    height: u32
+    height: Option<u32>
 }
 
 impl Reading {
     fn get_risk_level(&self, map: &[[Option<Reading>; 100]; 100]) -> u32 {
         // if it's the lowest of all it's neighbors, return height+1,  else fall to returning 0
         if self.is_lowpoint(map) {
-            return self.height + 1;
+            return self.height.unwrap() + 1;
         }
         0 // else no risk
     }
@@ -149,18 +156,18 @@ impl Reading {
         let mut return_val = Vec::new();
         // top
         if self.y > 0 {
-            return_val.push(Reading { x: self.x, y: self.y - 1, height: 0 });
+            return_val.push(Reading { x: self.x, y: self.y - 1, height: None });
         }
         // bottom
         if self.y < 99 {
-            return_val.push(Reading { x: self.x, y: self.y + 1, height:  0 });
+            return_val.push(Reading { x: self.x, y: self.y + 1, height:  None });
         }
         // left
         if self.x > 0 {
-            return_val.push(Reading { x: self.x - 1, y: self.y, height: 0 });
+            return_val.push(Reading { x: self.x - 1, y: self.y, height: None });
         }
         if self.x < 99 {
-            return_val.push(Reading { x: self.x + 1, y: self.y, height: 0 });
+            return_val.push(Reading { x: self.x + 1, y: self.y, height: None });
         }
         // right
         return_val
